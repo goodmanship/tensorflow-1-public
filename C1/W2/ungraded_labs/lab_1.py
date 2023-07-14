43548b5953ab29daf36dcafb51589cec9565f130
Rio Goodman <goodmanship@laroche.lan>
2023-07-14 12:10:02 -0500

    week 2 complete

---
+++ C1/W2/ungraded_labs/lab_1.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy') >= 0.85): # Experiment with changing this value
+      print("\nReached 85% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
+
+# Load the Fashion MNIST dataset
+fmnist = tf.keras.datasets.fashion_mnist
+
+# Load the training and test split of the Fashion MNIST dataset
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+
+# # -----
+# # You can put between 0 to 59999 here
+# index = 392
+
+# # Set number of characters per row when printing
+# np.set_printoptions(linewidth=320)
+
+# # Print the label and image
+# print(f'LABEL: {training_labels[index]}')
+# print(f'\nIMAGE PIXEL ARRAY:\n {training_images[index]}')
+
+# # Visualize the image
+# plt.imshow(training_images[index])
+
+# Normalize the pixel values of the train and test images
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+# Build the classification model
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# # Declare sample inputs and convert to a tensor
+# inputs = np.array([[1.0, 3.0, 4.0, 2.0]])
+# inputs = tf.convert_to_tensor(inputs)
+# print(f'input to softmax function: {inputs.numpy()}')
+
+# # Feed the inputs to a softmax activation function
+# outputs = tf.keras.activations.softmax(inputs)
+# print(f'output of softmax function: {outputs.numpy()}')
+
+# # Get the sum of all values after the softmax
+# sum = tf.reduce_sum(outputs)
+# print(f'sum of outputs: {sum}')
+
+# # Get the index with highest value
+# prediction = np.argmax(outputs)
+# print(f'class with highest probability: {prediction}')
+
+model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
+
+# Evaluate the model on unseen data
+model.evaluate(test_images, test_labels)
+
+# Exercise 1
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0]) # hint
